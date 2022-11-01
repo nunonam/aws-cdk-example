edfda9a596106047cf85d730deedd461713785c6
nunonam <nunonam@gmail.com>
2022-11-01 20:20:00 +0900

    first commit

--- lib/hello-world-stack.ts
+++ lib/hello-world-stack.ts
@@ -1,16 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { HelloWorld } from './hello-world';
 
-export class HelloWorldStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class HelloWorldStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloWorldQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new HelloWorld(this, 'hello-world');
   }
 }
